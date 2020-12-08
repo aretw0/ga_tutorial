414fe51ef3ec9f6176205326dbe63de81e818762
Arthur Aleksandro Alves Silva-ALVESSIL <alvessil@motorola.com>
2020-12-08 20:54:57 -0300

    Bulk files

---
+++ src/test/java/br/ufpe/cinmoto/ga_tutorial/app/AppTest.java
@@ -0,0 +1,20 @@
+package br.ufpe.cinmoto.ga_tutorial.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
